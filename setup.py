f7a165d6669724accd924d44c34cd26177a53e17
Kevin Musgrave <kevin.musgrave@naturalgasintel.com>
2024-01-24 14:11:25 -0600

    Renamed the project from wagtailautocomplete to ngiautocomplete.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 from os import path
-from wagtailautocomplete import __version__
+from ngiautocomplete import __version__
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as f:
@@ -8,16 +8,16 @@ with open(path.join(here, 'README.rst')) as f:
 
 
 setup(
-    name='wagtail-autocomplete',
+    name='ngi-autocomplete',
     version=__version__,
 
     packages=find_packages(),
     include_package_data=True,
 
-    description='An Autocomplete edit handler for Pages, Snippets, and more.',
+    description='An Autocomplete edit handler for Pages, Snippets, and more. Forked from wagtail-autocomplete',
     long_description=long_description,
 
-    url='https://github.com/wagtail/wagtail-autocomplete',
+    url='https://github.com/kevin-ngi/ngi-autocomplete',
 
     author='Emily Horsman and Harris Lapiroff',
     author_email='me@emilyhorsman.com',
